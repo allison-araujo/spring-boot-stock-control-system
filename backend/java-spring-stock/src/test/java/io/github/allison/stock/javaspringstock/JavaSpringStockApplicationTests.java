15e6b0237eee3221b765fca8e0dc4f6860e0766d
allisonvinicius <allisonvinicius63@gmail.com>
2023-07-12 21:46:22 -0400

    feat: config initial application

---
+++ backend/java-spring-stock/src/test/java/io/github/allison/stock/javaspringstock/JavaSpringStockApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.allison.stock.javaspringstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
